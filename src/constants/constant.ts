246cd3c4c2baa2a0a747dbcdcf59706b23db2324
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-08 23:23:36 +0100

    add globalstyles to constant

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,4 +1,4 @@
-import React, {Platform, StatusBar} from 'react-native';
+import React, {Platform, StatusBar, StyleSheet} from 'react-native';
 import {Dimensions} from 'react-native';
 
 export const STATUSBAR_HEIGHT = StatusBar.currentHeight;
@@ -6,3 +6,15 @@ export const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
 
 export let deviceWidth = Dimensions.get('window').width;
 export let deviceHeight = Dimensions.get('window').height;
+
+export const globalStyles = StyleSheet.create({
+  textPrimary: {
+    color: '#4c4c4c',
+  },
+  textGreen: {
+    color: 'green',
+  },
+  textWhite: {
+    color: '#fff',
+  },
+});
