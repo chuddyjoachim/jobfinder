fe64cef2862169f1391cffe10ab7919ffdef12a8
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-08 23:23:54 +0100

    add dummy home map component

---
+++ src/modules/destination/DestinationController.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const DestinationController = () => {
+    return (
+        <View>
+            <Text>DestinationController page</Text>
+        </View>
+    )
+}
+
+export default DestinationController
+
+const styles = StyleSheet.create({})
