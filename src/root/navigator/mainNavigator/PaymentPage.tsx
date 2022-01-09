dcebe7e08736a30253d3669c5622d4ae54b2d4f2
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-09 12:38:50 +0100

    created payment page

---
+++ src/root/navigator/mainNavigator/PaymentPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import PaymentController from '../../../modules/payment/PaymentController';
+
+const PaymentPage = () => {
+  return <PaymentController />;
+};
+
+export default PaymentPage;
+
+const styles = StyleSheet.create({});
