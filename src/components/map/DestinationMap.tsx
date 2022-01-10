bfe28c756296ca126475892af228d3950a851e29
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-10 09:17:30 +0100

    add markers to map screens

--- src/components/map/DestinationMap.tsx
+++ src/components/map/DestinationMap.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapMarker from './MapMarker';
 
 const DestinationMap = () => {
+  const [{mapLat, mapLng}, setLatLngMap] = React.useState({
+    mapLat: 6.5818,
+    mapLng: 3.3211,
+  });
   return (
     <>
-      <StatusBar translucent backgroundColor={"transparent"}/>
+      <StatusBar translucent backgroundColor={'transparent'} />
       <View style={styles.mapContainer}>
-        <Text>Destination map</Text>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={{width: '100%', height: '100%'}}
+          initialRegion={{
+            latitude: mapLat,
+            longitude: mapLng,
+            latitudeDelta: 0.008522,
+            longitudeDelta: 0.047431,
+          }}>
+          {/* adding maarkers */}
+
+          <Marker coordinate={{latitude: mapLat, longitude: mapLng}}>
+            <View style={{
+              width: 50,
+              height: 50,
+              backgroundColor:'#000',
+              borderRadius: 1000,
+              borderWidth: 4,
+              borderColor: "#fff"
+            }}/>
+          </Marker>
+          <MapMarker latitude={mapLat + 0.00145} longitude={mapLng + 0.00411} />
+        </MapView>
       </View>
     </>
   );
--- src/components/map/HomeMap.tsx
+++ src/components/map/HomeMap.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import {
+  Image,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapMarker from './MapMarker';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const HomeMap = () => {
-  const [{mapLat, mapLng}, setLatLngMap] = React.useState({mapLat: 6.5818, mapLng: 3.3211});
+  const [{mapLat, mapLng}, setLatLngMap] = React.useState({
+    mapLat: 6.5818,
+    mapLng: 3.3211,
+  });
   return (
     <>
       <StatusBar translucent backgroundColor={'transparent'} />
       <View style={styles.mapContainer}>
-        {/* <Text>map</Text> */}
-        {/* <MapView/> */}
+        <Pressable
+          onPress={() => {}}
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 1000,
+            backgroundColor: '#fff',
+            position: 'absolute',
+            top: 40,
+            left: 25,
+            zIndex: 100,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Entypo name="menu" size={40} color="#191919" />
+        </Pressable>
         <MapView
           provider={PROVIDER_GOOGLE}
           style={{width: '100%', height: '100%'}}
@@ -17,9 +44,22 @@ const HomeMap = () => {
             latitude: mapLat,
             longitude: mapLng,
             latitudeDelta: 0.008522,
-						longitudeDelta: 0.047431,
-          }}
-        />
+            longitudeDelta: 0.047431,
+          }}>
+          {/* adding maarkers */}
+          <MapMarker latitude={mapLat} longitude={mapLng} />
+          <MapMarker latitude={mapLat + 0.0145} longitude={mapLng + 0.00911} />
+          <MapMarker latitude={mapLat + 0.02065} longitude={mapLng + 0.00991} />
+          <MapMarker latitude={mapLat - 0.0232} longitude={mapLng - 0.0139} />
+          <MapMarker latitude={mapLat - 0.000232} longitude={mapLng - 0.0139} />
+          <MapMarker latitude={mapLat - 0.008232} longitude={mapLng - 0.0139} />
+          <MapMarker latitude={mapLat - 0.009232} longitude={mapLng - 0.0169} />
+          <MapMarker latitude={mapLat + 0.009232} longitude={mapLng - 0.0169} />
+          <MapMarker
+            latitude={mapLat + 0.009232}
+            longitude={mapLng - 0.00469}
+          />
+        </MapView>
       </View>
     </>
   );
